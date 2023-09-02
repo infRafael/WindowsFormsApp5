961a336791c77656c23a40dd265faa2a1619253e
Rafael Augusto <98691559+infRafael@users.noreply.github.com>
2023-09-02 08:20:28 -0300

    Add files via upload

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 92);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(243, 92);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(216, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(12, 170);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.ShortcutsEnabled = false;
+            this.textBox3.Size = new System.Drawing.Size(447, 252);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "PREÇO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(239, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PARCELAS";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(447, 40);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "COMPRAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(504, 218);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(237, 51);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "PAGAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(502, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(200, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "DIA DO PAGAMENTO";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(501, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(183, 58);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "VALOR TOTAL:\nR$0,00";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(502, 285);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(154, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Ultima parcela paga:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(506, 186);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(235, 26);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,145 @@
+﻿/* *******************************************************************
+* Colegio Técnico Antônio Teixeira Fernandes (Univap)
+* Curso Técnico em Informática - Data de Entrega: 01/09/2023
+* Autores do Projeto: Gabriel Costa Fileno
+*                     Rafael Augusto Guimarães da Silva
+* Turma: 2H
+* Atividade Proposta em aula
+* Observação: <colocar se houver>
+* 
+* problema_aula.cs
+* 
+* Funcionalidades dos componentes:
+*   textBox1 -> Caixa de texto para a entrada do preço.
+*   textBox2 -> Caixa de texto para a entrada do número de parcelas.
+*   textBox3 -> Caixa de texto multilinha para envio dos resultados.
+*   button1 -> Botão para a execução da rotina de compra.
+*   button2 -> Botão para a execução da rotina de pagamento.
+*   dateTimePicker1 -> Seletor de data para a entrada do dia do pagamento.
+*   label1 -> Rótulo para informar o que deve ser inserido na textbox correspondente.
+*   label2 -> Rótulo para informar o que deve ser inserido na textbox correspondente.
+*   label3 -> Rótulo para informar o que deve ser inserido na textbox correspondente.
+*   label4 -> Rótulo para a saída do valor total a ser pago.
+*   label5 -> Rótulo para a saída do valor da última parcela, com reajuste de juros se nescessário.
+* ************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        decimal valor_total = 0;
+        int numero_parcelas = 0;
+        int parcelas_pagas = 0;
+        DateTime data_compra;
+        decimal valor_compra;
+        decimal diferenca = 0;
+        decimal valor_parcelas;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+            parcelas_pagas = 0;
+            data_compra = DateTime.Now;
+            data_compra = new DateTime(data_compra.Year, data_compra.Month, data_compra.Day, 0, 0, 0);
+            numero_parcelas = int.Parse(textBox2.Text);
+            valor_compra = Math.Round(decimal.Parse(textBox1.Text), 2);
+            valor_parcelas = valor_compra / numero_parcelas;
+            valor_parcelas = Math.Truncate(valor_parcelas * 100) / 100;
+            diferenca = (valor_compra - (valor_parcelas * numero_parcelas))*100;
+            
+            for (int i = 1; i <= numero_parcelas; i++)
+            {
+                DateTime data_parcela = data_compra.AddMonths(i);
+                int dia = (int) data_parcela.DayOfWeek;
+                if (dia == 0)
+                    data_parcela = data_parcela.AddDays(1);
+                else if (dia == 6)
+                    data_parcela = data_parcela.AddDays(2);
+                string infos_parcela;
+                if (i <= diferenca)
+                {
+                    decimal valor_parcelas_reajuste = valor_parcelas + 0.01M;
+                    infos_parcela = i.ToString() + "ª Parcela - Vencimento: " + data_parcela.ToString("dd/MM/yy") + " - Valor: R$" + valor_parcelas_reajuste.ToString("0.00") + Environment.NewLine;
+                }
+                else
+                {
+                    infos_parcela = i.ToString() + "ª Parcela - Vencimento: " + data_parcela.ToString("dd/MM/yy") + " - Valor: R$" + valor_parcelas.ToString("0.00") + Environment.NewLine;
+                }
+
+                textBox3.AppendText(infos_parcela);
+            }
+            valor_total = valor_compra;
+            label4.Text = "VALOR TOTAL:" + Environment.NewLine + "R$" + valor_total.ToString("0.00"); 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime data_pagamento = dateTimePicker1.Value;
+            data_pagamento = new DateTime(data_pagamento.Year,data_pagamento.Month, data_pagamento.Day, 0, 0, 0);
+            if (parcelas_pagas != numero_parcelas && data_pagamento >= data_compra)
+            {
+                decimal valor_parcelas_reajuste = 0;
+                textBox3.Text = "";
+                parcelas_pagas++;
+
+                string infos_pagamento;
+                for (int i = parcelas_pagas; i <= numero_parcelas; i++)
+                {
+                     DateTime data_parcela = data_compra.AddMonths(i);
+                     int dia = (int) data_parcela.DayOfWeek;
+                     if (dia == 0)
+                        data_parcela = data_parcela.AddDays(1);
+                     else if (dia == 6)
+                        data_parcela = data_parcela.AddDays(2);
+
+                    if (i <= diferenca)
+                       valor_parcelas_reajuste = valor_parcelas + 0.01M;
+                    else
+                       valor_parcelas_reajuste = valor_parcelas;                   
+
+                     if (i == parcelas_pagas)
+                     {
+                        valor_total -= valor_parcelas_reajuste;
+               
+                        if (data_pagamento <= data_parcela)
+                        {
+                            infos_pagamento = "Ultima parcela paga: R$" + valor_parcelas_reajuste.ToString("0.00") + Environment.NewLine + "Sem juros.";
+                            label5.Text = infos_pagamento;
+                        }
+                        else
+                        {
+                            decimal parcela_juros = valor_parcelas_reajuste * 1.03M;
+                            infos_pagamento = "Ultima parcela paga: R$" + parcela_juros.ToString("0.00") + Environment.NewLine + "Parcela em atraso, juros de 3%.";
+                            label5.Text = infos_pagamento;
+                        }
+                     }
+                     else
+                     {
+                        string infos_parcela = i.ToString() + "ª Parcela - Vencimento: " + data_parcela.ToString("dd/MM/yy") + " - Valor: R$" + valor_parcelas_reajuste.ToString("0.00") + Environment.NewLine;
+                        textBox3.AppendText(infos_parcela);
+                     }
+                        
+                }
+                label4.Text = "VALOR TOTAL:" + Environment.NewLine + "R$" + valor_total.ToString("0.00");
+            }
+            else
+            {
+                label5.Text = "O pagamento é impossível de ser realizado.";
+            }
+        }
+    }
+}
